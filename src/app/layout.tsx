9cfbcd0db9daae69f2b169f40871f0d3bca55b9d
Emilm76 <emilm7632@gmail.com>
2025-08-10 12:08:02 +0300

    Добавил Яндекс Метрику

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
+import Script from 'next/script';
 import './globals.scss';
 
 export const metadata: Metadata = {
@@ -19,6 +20,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" data-scroll-behavior="smooth">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive"></Script>
+        <script type="text/javascript">
+          {`
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103654771', 'ym');
+
+    ym(103654771, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+    `}
+        </script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103654771"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
+
       <body className={roboto.className}>{children}</body>
     </html>
   );
